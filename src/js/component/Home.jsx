e88513125d17b4b01acfbb8e525c3ca310b0bec9
Atulhom <joanguaschduran@gmail.com>
2023-11-30 13:30:14 +0000

    no state&remove ID

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,34 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "./Navbar.jsx";
 import Card from "./Card.jsx";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 
-//include images into your bundle
+// include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+// create your first component
 const Home = () => {
+  const cards = [1,2,3,4];
 
-	const [cards] = useState([
-		{id: 1, name: 'javascript 1'},
-		{id: 2, name: 'card 2'}
-	]);
-	
-	return (
-		<div className="text-center">
-			<Header />
-			<Navbar />
-			<div className="d-flex gap-4 justify-content-centers mt-4">
-				{cards.map((item)=>{
-						return <Card title={item.name} key={item.id}/>
-					}
-				)}
-			</div>
-			
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <Header />
+      <Navbar />
+	  <div className="d-flex gap-4 justify-content-center mt-4">
+        {cards.map((index) => (
+          <Card key={index} />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
