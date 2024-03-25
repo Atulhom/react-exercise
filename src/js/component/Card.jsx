17e5b05dbb305334dc6a2dd43bbb9791c5998ff8
Atulhom <joanguaschduran@gmail.com>
2024-03-25 05:59:23 +0000

    responsive

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -3,7 +3,10 @@ import React from "react";
 //create your first component
 const Card = ({ title }) => {
   return (
-    <div className="card col-3" style={{ width: "18rem" }}>
+    <div
+      className="card col-3 col-sm-12 col-md-6 col-lg-4 mb-3"
+      style={{ width: "18rem" }}
+    >
       <img
         src="https://rsjlawang.com/assets/images/placehold_it/500x300.png"
         className="card-img-top"
